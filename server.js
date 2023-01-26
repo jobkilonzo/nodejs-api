0cf0884b4599a26d6b9dce700c7f3e4d931b4f4b
job kilonzo <jobkilonzo95@gmail.com>
2023-01-26 09:37:36 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const PORT  = process.env.PORT || 3000
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log("Connected to Database"))
+
+app.use(express.json())
+const subscribersRouter = require("./routes/subscribers")
+app.use("/subscribers", subscribersRouter)
+app.listen(PORT, () => console.log("Connected to server"))
